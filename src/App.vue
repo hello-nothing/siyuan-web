b0fb319f7968eb99b9f145f12a17071be53310d8
15037268535@sina.cn <15037268535@sina.cn>
2023-01-11 22:42:35 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <router-view />
+    <!-- <Footer></Footer> -->
+  </div>
+</template>
+
+<script>
+// import Footer from "@/components/footer.vue";
+export default {
+  name: "App",
+  components: {
+    // Footer
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #333333;
+}
+body{
+  margin: 0;
+}
+</style>
